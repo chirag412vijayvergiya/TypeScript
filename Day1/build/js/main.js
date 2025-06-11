7d33da065df5b47fc2ee2498a578fe90bf167438
Chirag vijayvergiya <111374446+chirag412vijayvergiya@users.noreply.github.com>
2025-06-11 19:32:25 +0530

    Just starting

---
+++ Day1/build/js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+let username = 'chirag';
+console.log(username);
+let a = 3;
+let b = 5;
+let c = '4';
+console.log(a / b);
+// console.log(a / c);
+// "noEmitOnError": true,  :-  this flag will not ampile javascript
---
+++ Day1/src/main.ts
@@ -0,0 +1,12 @@
+let username = 'chirag';
+console.log(username);
+
+let a: number = 3;
+let b: number = 5;
+let c: string = '4';
+
+console.log(a / b);
+
+// console.log(a / c);
+
+// "noEmitOnError": true,  :-  this flag will not ampile javascript
